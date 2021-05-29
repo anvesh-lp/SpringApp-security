d19f5ebbeee3ec8dfdffa600096a5b1bb5a47c70
Naga Anvesh <knanvesh.01@gmail.com>
2021-05-29 17:49:21 +0530

    Fixing Configuration Properties for intelli and code

--- src/main/java/com/anvesh/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/anvesh/springsecurity/SpringSecurityApplication.java
@@ -1,8 +1,12 @@
 package com.anvesh.springsecurity;
 
+import com.anvesh.springsecurity.jwt.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({JwtConfig.class
+})
 @SpringBootApplication
 public class SpringSecurityApplication {
 
